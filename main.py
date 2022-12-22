093b36d67ba34fa9f6504029d80b28535daff26d
Smith Oliveira <olindex@gmail.com>
2022-12-22 19:31:17 +0000

    Remove unnecessary lines.

--- main.py
+++ main.py
@@ -55,17 +55,16 @@ def func_args(folder, remove_domain_name=''):
 
     e.g.
         source /venv/bin/python/activate
-        python main.py -u developer -p C1sco12345 -e C1sco12345 -f start_here.yml
+        python main.py -u developer -p C1sco12345 -e C1sco12345 -f hosts.yml
 
     Creating the yaml file:
 
         vim hosts.yml (you can give any name, as long as it is a yaml format.)
 
         ---
-        host_oliveiras:
-          - 192.168.56.150
-          - 192.168.56.160
-          - 192.168.56.170
+        host:
+          - sandbox-iosxe-recomm-1.cisco.com
+          - sandbox-iosxe-latest-1.cisco.com
 
     Remember:
         You don't have to shuffle the file.
@@ -73,29 +72,15 @@ def func_args(folder, remove_domain_name=''):
         You don't have to login to the device directly.
     '''
 
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description=arg_desc)
-
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=arg_desc)
     parser.add_argument('-u', '--username', help='Enter username.', type=str, required=True)
     parser.add_argument('-p', '--password', help='Enter password.', type=str, required=True)
     parser.add_argument('-e', '--enable', help='Enter enable password.', type=str, required=True)
-    # parser.add_argument('-d', '--device_type', help='Enter device type. e.g. cisco_ios, cisco_nxos, cisco_asa',
-    #                     type=str, required=True)
     parser.add_argument('-f', '--file', help='Enter config file name (yml).', type=str, required=True)
-    # parser.add_argument('-g', '--group', help='Enter group name.', type=str, required=True)
-
     args = parser.parse_args()
-
-    args_cred = {
-        'username': args.username,
-        'password': args.password,
-        'secret': args.enable
-    }
-
-    # args_host = func_yml(args.file, args.group)
+    args_cred = {'username': args.username, 'password': args.password, 'secret': args.enable}
     host = func_yml(args.file, 'host')
     parameters = func_yml(args.file, 'parameters')
-
     task = Device(args_cred, host, parameters.get('device_type'))
     task.backup(folder, remove_domain_name)
 
@@ -113,7 +98,6 @@ def func_input(folder, remove_domain_name=''):
     }
     host = func_yml(config_file, 'host')
     parameters = func_yml(config_file, 'parameters')
-
     task = Device(cred, host, parameters.get('device_type'))
     task.backup(parameters.get(folder), remove_domain_name)
 
@@ -142,7 +126,7 @@ class Login:
                 # 'global_delay_factor': 2,
                 # "read_timeout_override": 90,
                 # 'banner_timeout': 20,
-                'session_log': 'output_high-level.log'
+                # 'session_log': 'output_high-level.log'
             }
 
             try:
@@ -150,9 +134,6 @@ class Login:
                 net_connect = ConnectHandler(**device)
                 net_connect.enable()
                 self.output.append(net_connect)
-
-            # except exceptions as error:
-            #     print(error)
 
             except exceptions:
                 print(f'{hosts}: failed')
@@ -164,19 +145,6 @@ class Login:
 class Device(Login):
     def __init__(self, var_credentials, var_hosts, var_device_type):
         super().__init__(var_credentials, var_hosts, var_device_type)
-
-    def save(self):
-        net_connect = self.login()
-
-        output = []
-        for net in net_connect:
-            try:
-                display = net.save_config()
-                output.append(display)
-            except exceptions as error:
-                print(error)
-
-        return output
 
     def backup(self, folder, remove_domain_name=''):
         net_connect = self.login()
@@ -212,13 +180,13 @@ class Device(Login):
 
 def main():
     # Use Command-Line Arguments
-    # func_args('tftp', '.ucl.ac.uk')
+    # func_args('tftp', '.xxx.ac.uk')
 
     # Use Prompt the User for Input
-    # func_input('folder', '.ucl.ac.uk')
+    # func_input('folder', '.xxx.ac.uk')
 
     # Use The start_here.yml File
-    func_start_here('folder', '.ucl.ac.uk')
+    func_start_here('folder', '.xxx.ac.uk')
 
 
 if __name__ == '__main__':
